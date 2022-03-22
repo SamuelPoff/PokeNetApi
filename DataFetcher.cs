60dd19f313a4c671e4b294cf97a3571587ca0e49
SamuelPoff <samuelpoff21@gmail.com>
2022-03-22 18:47:13 -0400

    Added Pokemon data fetching

---
+++ DataFetcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using System.Text.Json;
+
+using PokeNetApi.Objects;
+
+namespace PokeNetApi
+{
+    public class DataFetcher
+    {
+
+        HttpBackend backend = new HttpBackend();
+
+        public DataFetcher()
+        {
+
+        }
+
+        public async Task<string> GetDataAsync(string path)
+        {
+
+            return await backend.GetStringAsync(path);
+
+        }
+
+        public async Task<Pokemon> GetPokemonAsync(string path)
+        {
+
+            Stream jsonStream = await backend.GetStreamAsync(path);
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            Pokemon pokemon = await JsonSerializer.DeserializeAsync<Pokemon>(jsonStream, options);
+            return pokemon;
+
+        }
+
+    }
+}
---
+++ HttpBackend.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+using System.Threading.Tasks;
+
+namespace PokeNetApi
+{
+    public class HttpBackend
+    {
+
+        public static readonly string DefaultApiSiteURL = "https://pokeapi.co";
+        public static readonly string DefaultBaseURL = $"{DefaultApiSiteURL}/api/v2/";
+        public static readonly string UserAgent = "PokeNetApi (Testing new class library for interacting with PokeApi)";
+
+        private HttpClient client;
+
+        public HttpBackend()
+        {
+
+            client = new HttpClient();
+            client.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
+
+        }
+
+        public async Task<Stream> GetStreamAsync(string path)
+        {
+
+            string url = (path.Contains(DefaultBaseURL)) ? path : DefaultBaseURL + path;
+            return await client.GetStreamAsync(url);
+
+        }
+
+        public async Task<string> GetStringAsync(string path)
+        {
+
+            string url = (path.Contains(DefaultBaseURL)) ? path : DefaultBaseURL + path;
+            return await client.GetStringAsync(url);
+
+        }
+
+    }
+}
